7226726a7d315c169d3866bb4025b97fa50f71a0
fernandogross06 <80482002+fernandogross06@users.noreply.github.com>
2024-05-04 20:37:52 -0600

    Creacion de pantalla productos
    Creacion de la pantalla de productos del admin

--- back-end/logIn.js
+++ back-end/logIn.js
@@ -1,3 +1,8 @@
 function registraUsuario(){
-    location.href = "../../fron-end/archivosHTML/landingRegistrado.html"
+    if(document.getElementById("usuario").value == "admin"){
+        location.href = "../../fron-end/archivosHTML/landingAdmin.html"
+    }else{
+        location.href = "../../fron-end/archivosHTML/landingRegistrado.html"
+    }
+    
 }

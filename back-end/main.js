d2203bdd4ab86ae51482815f0f8c120e52cd1b1b
fernandogross06 <80482002+fernandogross06@users.noreply.github.com>
2024-05-05 01:27:53 -0600

    Creacion de mas pantallas
    Creacion de las pantallas de historial, carrito, wishlist y parte de perfil

--- back-end/main.js
+++ back-end/main.js
@@ -1,1 +1,8 @@
-prueba
+function pagar(){
+    
+    location.href = "../../fron-end/archivosHTML/pago.html"
+}
+
+function volverPagar(){
+    location.href = "../../fron-end/archivosHTML/carrito.html"
+}

a480dd83c6ec6ae36074b31c94c19482c37dfc73
fernandogross06 <80482002+fernandogross06@users.noreply.github.com>
2024-05-06 02:03:30 -0600

    Agregado de mas pantallas
    Se agregaron perfil, modificar perfil y modificar producto

--- back-end/main.js
+++ back-end/main.js
@@ -6,3 +6,13 @@ function pagar(){
 function volverPagar(){
     location.href = "../../fron-end/archivosHTML/carrito.html"
 }
+
+function volverPerfil(){
+    console.log("entra")
+    
+    location.href = "../../fron-end/archivosHTML/perfil.html"
+}
+
+function editarPerfil(){
+    location.href = "../../fron-end/archivosHTML/editarPerfil.html"
+}

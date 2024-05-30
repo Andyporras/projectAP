0bd3e4f5b9b37884f9c06fb08e995daad7ed72a6
cherry_motion <dlmora348@gmail.com>
2024-05-30 13:28:06 -0600

    filtro estadisticas

--- ProjectAP/public/js/estadisticas.js
+++ ProjectAP/public/js/estadisticas.js
@@ -1,45 +1,75 @@
 
 
 
-function cargaGraficos(){
+function cargaGraficos(flag){
 
     console.log("SI ENTRO");
-
-    
-
-    var pie = new ej.charts.AccumulationChart({
-        //Initializing Series
-        series: [
-            {
-                
-                dataSource: [
+    document.getElementById("containers").innerHTML = "";  
+    document.getElementById("container").innerHTML = "";
+      
+    if(flag == 1|| flag == null){
+        var pie = new ej.charts.AccumulationChart({
+            //Initializing Series
+            series: [
+                {
                     
-                    { 'x': 'Hombres', y: 65, }, 
-                    { 'x': 'Mujeres', y: 30, },
-                    { 'x': 'Otro', y: 5 }
-                ],
-                dataLabel: {
-                    visible: true,
-                    position: 'Inside',
-                },
-                xName: 'x',
-              
-                yName: 'y'
+                    dataSource: [
+                        
+                        { 'x': 'Hombres', y: 65, }, 
+                        { 'x': 'Mujeres', y: 30, },
+                        { 'x': 'Otro', y: 5 }
+                    ],
+                    dataLabel: {
+                        visible: true,
+                        position: 'Inside',
+                    },
+                    xName: 'x',
                 
-            }
+                    yName: 'y'
+                    
+                }
+                
+            ],
+            background: "#fffa",
+            width: '650', height: '350'
             
-        ],
-        background: "#fffa",
-        width: '650', height: '350'
-        
-        
-    });
+            
+        });
+    }else{
+        var pie = new ej.charts.AccumulationChart({
+            //Initializing Series
+            series: [
+                {
+                    
+                    dataSource: [
+                        
+                        { 'x': 'Costarricense', y: 3, }, 
+                        { 'x': 'Americano', y: 1, }
+                    ],
+                    dataLabel: {
+                        visible: true,
+                        position: 'Inside',
+                    },
+                    xName: 'x',
+                
+                    yName: 'y'
+                    
+                }
+                
+            ],
+            background: "#fffa",
+            width: '650', height: '350'
+            
+            
+        });
+
+    }
     pie.appendTo('#container');
 
 
     
 
-   
+    if (flag == 1){
         var data = {
             header: ["Name", "Death toll"],
             rows: [
@@ -48,7 +78,14 @@ function cargaGraficos(){
               ["Otro", 5]
           ]}
     
-
+    }else{
+        var data = {
+            header: ["Name", "Death toll"],
+            rows: [
+              ["Costarricense", 3],
+              ["Americano", 1]
+          ]}
+    }
     // create the chart
 var chart = anychart.column();
  

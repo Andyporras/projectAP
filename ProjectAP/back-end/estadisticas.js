cf16e5ebf9f014771e90cf022adae1a4469ce36f
cherry_motion <dlmora348@gmail.com>
2024-05-30 12:12:11 -0600

    formatear fecha

--- ProjectAP/back-end/estadisticas.js
+++ ProjectAP/back-end/estadisticas.js
@@ -5,15 +5,23 @@ const { post } = require('./login');
 
 // funciones de estadisticas
 router.get('/estadisticas', (req, res) => {
-    conexion.query('SELECT tableName as TABLA, modification as ACCION, oldValue AS VALORES_ANTIGUOS, newValue AS VALORES_NUEVOS, modificationDate FROM records order by id desc', (error, results) => {
+    conexion.query('SELECT tableName as TABLA, modification as ACCION, oldValue AS VALORES_ANTIGUOS, newValue AS VALORES_NUEVOS, modificationDate as FECHA FROM records order by id desc', (error, results) => {
         if (error) {
           console.error('Error en la consulta: ', error);
           return res.status(500).json({ error: 'Error en la consulta' });
         }
-  
-        res.render('estadisticas', {datosBi: results});
+
+        const formattedResults = results.map(record => {
+          return {
+              ...record,
+              FECHA: new Date(record.FECHA) // Cambia 'fechaCampo' al nombre de tu campo de fecha
+          };
+        });
+        console.log(formattedResults);
+        res.render('estadisticas', {datosBi: formattedResults});
 
     });
+
 
 });
 

b4abe9609cf59d1015d03a80030cb5fc1f820aa4
cherry_motion <dlmora348@gmail.com>
2024-05-30 11:47:57 -0600

    esta

--- ProjectAP/back-end/estadisticas.js
+++ ProjectAP/back-end/estadisticas.js
@@ -5,23 +5,13 @@ const { post } = require('./login');
 
 // funciones de estadisticas
 router.get('/estadisticas', (req, res) => {
-    conexion.query('SELECT tableName as TABLA, modification as ACCION, oldValue AS VALORES_ANTIGUOS, newValue AS VALORES_NUEVOS, modificationDate AS FECHA FROM records order by id desc', (error, results) => {
+    conexion.query('SELECT tableName as TABLA, modification as ACCION, oldValue AS VALORES_ANTIGUOS, newValue AS VALORES_NUEVOS, modificationDate FROM records order by id desc', (error, results) => {
         if (error) {
           console.error('Error en la consulta: ', error);
           return res.status(500).json({ error: 'Error en la consulta' });
         }
-      const datosBi =  results;
-      
-
-    conexion.query('call getEstadisticasPersonas(?, ?, ?);', [1, null, null],(error, resultado) => {
-        if (error) {
-          console.error('Error en la consulta: ', error);
-          return res.status(500).json({ error: 'Error en la consulta' });
-        }
-        const stats = resultado[0];
-
-        res.render('estadisticas', {datosBi: datosBi, stats: stats});
-      });
+  
+        res.render('estadisticas', {datosBi: datosBi});
 
     });
 
